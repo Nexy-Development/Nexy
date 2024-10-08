10013d682b1595c21aa6ba5e19f2c3d8259ae116
Floris29 <Florismelchers@gmail.com>
2024-10-08 09:18:43 +0200

    getting him online

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-const { Client, Intents } = require('discord.js');
+const { Client, Events, GatewayIntentBits  } = require('discord.js');
 const { presenceName, presenceType, presenceStatus } = require('./config.json');
-require('dotenv').config()
 
-Client.login(process.env.DISCORD_BOT_TOKEN);
+require('dotenv').config()
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+
+const DISCORD_BOT_TOKEN = process.env.DISCORD_BOT_TOKEN;
+client.login(DISCORD_BOT_TOKEN);
+
+client.once(Events.ClientReady, readyClient => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
