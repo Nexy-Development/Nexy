bdbb194dad1d4a2d3e44e06295ace187a67c816a
Floris29 <Florismelchers@gmail.com>
2024-10-10 09:35:45 +0200

    adding events to a events map with the documentation

---
+++ events/ready.js
@@ -0,0 +1,18 @@
+const { Events } = require("discord.js");
+
+module.exports = {
+  name: Events.ClientReady,
+  once: true,
+  execute(client) {
+    console.log(`Logged in as ${client.user.tag}`);
+    client.user.setPresence({
+      activities: [
+        {
+          name: "Everyone",
+          type: ActivityType.Watching,
+        },
+      ],
+      status: "dnd",
+    });
+  },
+};
--- index.js
+++ index.js
@@ -1,68 +1,33 @@
-const fs = require('node:fs');
-const path = require('node:path');
-const { Client, Collection, Events, GatewayIntentBits, ActivityType  } = require('discord.js');
+const fs = require("node:fs");
+const path = require("node:path");
+const { Client, Collection, GatewayIntentBits } = require("discord.js");
 
-require('dotenv').config()
+require("dotenv").config();
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 const token = process.env.token;
 client.login(token);
 
-client.on("ready", () => {
-	console.log(`Logged in as ${client.user.tag}!`);
-	client.user.setPresence({
-		activities: [{
-			name: 'EVERYONE',
-			type: ActivityType.Watching,
-		}],
-		status: 'dnd'
-	});
-    });
-
-
 client.commands = new Collection();
 
-const foldersPath = path.join(__dirname, 'commands');
+const foldersPath = path.join(__dirname, "commands");
 const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
-	const commandsPath = path.join(foldersPath, folder);
-	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-	for (const file of commandFiles) {
-		const filePath = path.join(commandsPath, file);
-		const command = require(filePath);
-		// Set a new item in the Collection with the key as the command name and the value as the exported module
-		if ('data' in command && 'execute' in command) {
-			client.commands.set(command.data.name, command);
-		} else {
-			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-		}
-	}
-}
+  const commandsPath = path.join(foldersPath, folder);
+  const commandFiles = fs
+    .readdirSync(commandsPath)
+    .filter((file) => file.endsWith(".js"));
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
 
-client.on(Events.InteractionCreate, interaction => {
-	if (!interaction.isChatInputCommand()) return;
-	console.log(interaction);
-});
-
-client.on(Events.InteractionCreate, async interaction => {
-	if (!interaction.isChatInputCommand()) return;
-
-	const command = interaction.client.commands.get(interaction.commandName);
-
-	if (!command) {
-		console.error(`No command matching ${interaction.commandName} was found.`);
-		return;
-	}
-
-	try {
-		await command.execute(interaction);
-	} catch (error) {	
-		console.error(error);
-		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-		} else {
-			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-		}
+    if ("data" in command && "execute" in command) {
+      client.commands.set(command.data.name, command);
+    } else {
+      console.log(
+        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+      );
     }
-});
+  }
+}
